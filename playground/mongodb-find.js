ab93976e4473a7e6d1ce4000260efa5af06e79e7
Deepak <deepak@Triconinfotech.com>
2018-10-29 12:22:00 +0530

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+// var obj = new ObjectID();
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to mongo db server');
+    }
+    console.log('Connected to server');
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch data ', err);
+    });
+
+    db.collection('Todos').find({
+        _id: new ObjectID('5bd6a45a0aa9161dd8eeff6a')
+    }).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 3));
+    }, (err) => {
+        console.log('Unable to fetch data ', err);
+    });
+
+    // db.close();
+});
