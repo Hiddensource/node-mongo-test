e4b94926fbf0d98428a00856ca025c2ad36770f4
Deepak <deepak@Triconinfotech.com>
2018-11-08 17:00:45 +0530

    11 nov

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,22 @@
-var mongosse = require('mongoose');
+const mongosse = require('mongoose');
+const validator = require('validator');
 
 var User = mongosse.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
+        trim: true,
+        unique: true,
+        validate: {
+            validator:validator.isEmail,
+            message: '{value} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        min 
     }
 });
 
